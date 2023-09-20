01d1f4a6fba916658223b6405b8b6928980b6c0d
Maxim Latnikov <latnikou@gmail.com>
2023-09-20 20:48:32 +0300

    feat: added main function to swap cases

---
+++ index.js
@@ -0,0 +1,10 @@
+const swapCase = (str) => {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const upper = str[i] === str[i].toUpperCase();
+    result += upper ? str[i].toLowerCase() : str[i].toUpperCase();
+  }
+  return result;
+};
+
+module.exports = swapCase;
